f5364c1b07543abec73360b8456c60633de0b8bb
ThiagoS04 <schuck.thiago@student.ccm.edu>
2023-12-09 22:17:39 -0500

    yelp analysis
    Scrape data from yelp.com and put business ratings into a chart

---
+++ Classwork/Yelp Scraping/yelpkeys.py
@@ -0,0 +1,7 @@
+"""
+Thiago Schuck December 6 2023
+API keys for Yelp Fusion API for Review Sentiment App
+"""
+
+clientId = "kMnX6yKjlI1z7FdxAqaC6A"
+apiKey = "qg8xCKSJAj_zNhxYPI82BRwYq-t9AvUs9D7eOYxEbPLBaaV3sv9K-Avmg8RErlijVvs6iCBvhjRijlLY_RjGHSN-B5u0VqhIIRDcQwqzcFmg-HdxF38Ziipyv5pwZXYx"
